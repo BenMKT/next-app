940289ae2edc3290b6c8dc0ee3806bdb27bde23a
Benson Kiai <benson.kiai86@gmail.com>
2024-01-29 08:28:50 +0300

    Refactor home page layout and add additional content with responsive styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,20 @@ import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
-    <main className='flex flex-col space-y-10'>      
-      <h1 className='mt-10 text-4xl'>Hello World!</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo corporis error quis facere excepturi iste, impedit praesentium ipsa! Nesciunt fugiat non doloremque inventore deserunt, nostrum neque impedit dignissimos quod itaque.</p>
-      < ProductCard />
+    <main className="flex flex-col space-y-10 px-7 md:max-w-3xl lg:max-w-4xl mx-auto">
+      <h1 className="mt-10 text-4xl font-semibold">Hello World!</h1>
+      <p className="text-xl text-gray-500 text-wrap">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        corporis error quis facere excepturi iste, impedit praesentium ipsa!
+        Nesciunt fugiat non doloremque inventore deserunt, nostrum neque impedit
+        dignissimos quod itaque.<br/><br/>
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        corporis error quis facere excepturi iste, impedit praesentium ipsa!
+        Nesciunt fugiat non doloremque inventore deserunt, nostrum neque impedit
+        dignissimos quod itaque.
+      </p>
+      <ProductCard />
     </main>
-  )
+  );
 }
